91c54208da7762250855913201148304e0f09f70
v-brkic <vinko.brkic337@gmail.com>
2024-10-22 16:58:50 +0200

    Middleware change

--- 06-auth-middleware.js
+++ 06-auth-middleware.js
@@ -1,27 +1,43 @@
 const jwt = require('jsonwebtoken');
+const jwksRsa = require('jwks-rsa'); // Use jwks-rsa to get Auth0 public keys
 const dotenv = require('dotenv');
 dotenv.config();
 
-// Function to create JWT token (not needed for Client Credentials Flow, but keeping for any user-based flows)
-function createToken(payload) {
-  return jwt.sign(payload, process.env.TOKEN_KEY, { expiresIn: "2m" });
+// Create a JWKS client to fetch public keys from Auth0
+const jwksClient = jwksRsa({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  cache: true,
+  rateLimit: true,
+  jwksRequestsPerMinute: 5
+});
+
+// Function to get the signing key from Auth0
+function getKey(header, callback) {
+  jwksClient.getSigningKey(header.kid, function (err, key) {
+    if (err) {
+      callback(err, null);
+    } else {
+      const signingKey = key.getPublicKey(); // Get the public key for verifying the token
+      callback(null, signingKey);
+    }
+  });
 }
 
 // Function to set user info from token in request object
 function setUserInfo(req, res, next) {
   const bearerToken = req.headers.authorization?.replace(/^Bearer /, '');
   if (bearerToken) {
-    try {
-      const token = jwt.verify(bearerToken, process.env.TOKEN_KEY);
-      // Assuming the token contains application-related data
-      if (token) {
-        req.user = { app: 'ticket-app' };  // You can store relevant info in req.user
+    jwt.verify(bearerToken, getKey, {}, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).send("Invalid Token");
       }
-    } catch (err) {
-      return res.status(401).send("Invalid Token");
-    }
+      // Assuming the token contains relevant application data
+      req.user = decodedToken;
+      next();
+    });
+  } else {
+    return res.status(401).send("Token not provided");
   }
-  next();
 }
 
 // Middleware to require authentication for protected routes
